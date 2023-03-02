d2a4a1adf7d39e8e40c6218701fb76c54c3bc6d7
snehaltawade <48874752+snehaltawade@users.noreply.github.com>
2023-03-02 21:04:11 +0530

    Update javascript.js
    added some more functions

--- javascript.js
+++ javascript.js
@@ -1,5 +1,13 @@
 //Displays entered input
+var flag=0;
 function display(val){
+   
+    if(val.toString().match(/^[-+*/]$/))
+        flag=0;
+    if(flag==1){
+        clearfun();
+        flag=0;
+    }
     document.getElementById("display").value+=val;
 }
 
@@ -7,12 +15,17 @@ function display(val){
 function clearfun(){
     document.getElementById("display").value='';
 }
-
+function clearLast(){
+    let expression=document.getElementById("display").value;
+    document.getElementById("display").value=expression.substr(0,expression.length-1);;
+}
 //Evaluates the expression
 function compute(e){
     let expression=document.getElementById("display").value;
-    let computedvalue=eval(expression);
+    let computedvalue=0;
+    computedvalue=eval(expression);
     document.getElementById("display").value=computedvalue?computedvalue:'';
+    flag=1;
     e?.preventDefault()
     
 }
@@ -29,3 +42,23 @@ addEventListener("keypress",function(event){
 
     }
 })
+
+function power(){
+   
+    let expression=document.getElementById("display").value
+    var splitArray=expression.split(/[-+*/]/);
+    computedvalue=Math.pow(parseInt(splitArray[splitArray.length-1]),2)
+    var str=splitArray.slice(0,splitArray.length-2).join();
+    var exp=str.concat(computedvalue);
+    document.getElementById("display").value=eval(exp);
+}
+
+function fraction(){
+   
+    let expression=document.getElementById("display").value
+    var splitArray=expression.split(/[-+*/]/);
+    computedvalue=1/(parseInt(splitArray[splitArray.length-1]))
+    var str=splitArray.slice(0,splitArray.length-2).join();
+    var exp=str.concat(computedvalue);
+    document.getElementById("display").value=eval(exp);
+}
